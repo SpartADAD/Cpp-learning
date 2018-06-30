5b7e81024ce0c0c0acbc9522361ed30a9929348a
SpartADAD <20164108@stu.neu.edu.cn>
2018-06-30 16:50:19 +0800

    vector 用 for(auto &r : v )实现元素翻倍

--- learning/learning/main.cpp
+++ learning/learning/main.cpp
@@ -3,21 +3,32 @@
 #include <string>
 using namespace std;
 using std::vector;
+//int main() {
+//	int cinTime = 0;
+//	string word;
+//	vector<string>text;
+//	cout << "!" << endl;
+//	while (cin >> word) {
+//		cinTime++;
+//		text.push_back(word);
+//		cout << word << endl;
+//		cout << "?" << endl;
+//		if (cinTime % 2 == 0) {
+//			for (vector<string>::size_type i = 0; i < text.size(); ++i) {
+//				cout << "这是列表内的数" << text[i] << endl;
+//			}
+//		}
+//	}
+//	getchar();
+//}
 int main() {
 	int cinTime = 0;
-	string word;
-	vector<string>text;
-	cout << "!" << endl;
-	while (cin >> word) {
-		cinTime++;
-		text.push_back(word);
-		cout << word << endl;
-		cout << "?" << endl;
-		if (cinTime % 2 == 0) {
-			for (vector<string>::size_type i = 0; i < text.size(); ++i) {
-				cout << "这是列表内的数" << text[i] << endl;
-			}
-		}
+	vector<int>vec={0,1,2};
+	for (auto &i : vec) {
+		i *= i;
+	}
+	for (auto i : vec) {
+		cout << i << vec[i] << endl;
 	}
 	getchar();
 }
